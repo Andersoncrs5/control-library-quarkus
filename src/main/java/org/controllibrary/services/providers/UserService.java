7e0f5a1367ab47120925d5edcd22749ba52048e5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-08 14:14:27 -0300

    New method create and existsByEmail

--- src/main/java/org/controllibrary/services/providers/UserService.java
+++ src/main/java/org/controllibrary/services/providers/UserService.java
@@ -1,33 +1,53 @@
 package org.controllibrary.services.providers;
 
-import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.controllibrary.dtos.users.CreateUserDTO;
 import org.controllibrary.models.UserModel;
 import org.controllibrary.repositories.UserRepository;
 import org.controllibrary.services.interfaces.IUserService;
+import org.controllibrary.utils.annotations.validations.emailConstraint.EmailConstraint;
 import org.controllibrary.utils.annotations.validations.isId.IsId;
 import org.controllibrary.utils.exceptions.ModelNotFoundException;
+import org.controllibrary.utils.mappers.UserMapper;
+import org.eclipse.microprofile.faulttolerance.Retry;
 
 @ApplicationScoped
 public class UserService implements IUserService {
 
-    @Inject
-    UserRepository userRepository;
+    @Inject UserRepository repository;
+    @Inject UserMapper mapper;
 
     @Override
     public Uni<UserModel> findById(@IsId Long id) {
-        return this.userRepository.findById(id)
+        return this.repository.findById(id)
                 .onItem()
                 .ifNull()
                 .failWith(() -> new ModelNotFoundException("User not found"));
     }
 
     @Override
-    @WithTransaction
-    public Uni<Boolean> delete(@IsId Long id) {
-        return this.userRepository.deleteById(id);
+    public Uni<Boolean> existsByEmail(@EmailConstraint String email) {
+        return this.repository.existsByEmail(email);
     }
+
+    @Retry
+    @Override
+    @Transactional
+    public Uni<Boolean> delete(@IsId Long id) {
+        return this.repository.deleteById(id);
+    }
+
+    @Retry
+    @Override
+    @Transactional
+    public Uni<UserModel> create(CreateUserDTO dto) {
+        UserModel model = this.mapper.toModel(dto);
+
+        return repository.persist(model);
+    }
+
 
 }
