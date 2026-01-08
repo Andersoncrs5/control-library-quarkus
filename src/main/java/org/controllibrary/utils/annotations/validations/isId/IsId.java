f4f8d932ef48b95919bf9072880599bd0443d42d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-08 13:56:07 -0300

    New annotation IsId to valid id

---
+++ src/main/java/org/controllibrary/utils/annotations/validations/isId/IsId.java
@@ -0,0 +1,21 @@
+package org.controllibrary.utils.annotations.validations.isId;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.lang.annotation.*;
+
+@NotNull(message = "The ID cannot be null.")
+@Positive(message = "The ID must be a positive number.")
+@Target({ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
+public @interface IsId {
+    String message() default "ID invalid";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
