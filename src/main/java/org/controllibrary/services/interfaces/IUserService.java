75dcf161e923c3cba653aa5a0945aee21fe4a257
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-08 14:14:36 -0300

    New method create and existsByEmail

--- src/main/java/org/controllibrary/services/interfaces/IUserService.java
+++ src/main/java/org/controllibrary/services/interfaces/IUserService.java
@@ -1,9 +1,14 @@
 package org.controllibrary.services.interfaces;
 
 import io.smallrye.mutiny.Uni;
+import org.controllibrary.dtos.users.CreateUserDTO;
 import org.controllibrary.models.UserModel;
+import org.controllibrary.utils.annotations.validations.emailConstraint.EmailConstraint;
+import org.controllibrary.utils.annotations.validations.isId.IsId;
 
 public interface IUserService {
-    Uni<UserModel> findById(Long id);
-    Uni<Boolean> delete(Long id);
+    Uni<UserModel> findById(@IsId Long id);
+    Uni<Boolean> delete(@IsId Long id);
+    Uni<Boolean> existsByEmail(@EmailConstraint String email);
+    Uni<UserModel> create(CreateUserDTO dto);
 }
