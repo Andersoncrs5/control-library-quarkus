307298c0e55b31cde6877f40a8c673d9a3257134
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-08 09:49:11 -0300

    Init project

---
+++ src/test/java/org/controllibrary/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.controllibrary;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
