726128a21d3eac1a72fff5c0628f9509c4e91e78
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-08 09:58:35 -0300

    New table UserModel

---
+++ src/main/java/org/controllibrary/models/UserModel.java
@@ -0,0 +1,42 @@
+package org.controllibrary.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.controllibrary.utils.base.BaseModel;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users", indexes = {
+        @Index(name = "idx_username", columnList = "username"),
+        @Index(name = "idx_name", columnList = "name"),
+        @Index(name = "idx_email", columnList = "email")
+})
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel extends BaseModel {
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "username", length = 200, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false, name = "email")
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    private Boolean active = true;
+
+    @Column(name = "blocked_at")
+    private LocalDateTime blockedAt;
+
+}
