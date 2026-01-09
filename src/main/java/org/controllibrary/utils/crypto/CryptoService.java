3084255593d74b9fa4389081064818c63ccbcbd8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-09 09:39:47 -0300

    Impled CryptoService

---
+++ src/main/java/org/controllibrary/utils/crypto/CryptoService.java
@@ -0,0 +1,18 @@
+package org.controllibrary.utils.crypto;
+
+import com.password4j.Password;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CryptoService {
+
+    public String encode(String rawPassword) {
+        return Password.hash(rawPassword)
+                .withArgon2()
+                .getResult();
+    }
+
+    public boolean verify(String rawPassword, String encodedPassword) {
+        return Password.check(rawPassword, encodedPassword).withArgon2();
+    }
+}
