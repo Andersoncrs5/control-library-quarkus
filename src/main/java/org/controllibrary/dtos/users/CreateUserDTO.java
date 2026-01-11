1125fc8ea3390e60506f327bf344d03adf7ba0b6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-10 21:22:13 -0300

    Added validations

--- src/main/java/org/controllibrary/dtos/users/CreateUserDTO.java
+++ src/main/java/org/controllibrary/dtos/users/CreateUserDTO.java
@@ -1,8 +1,13 @@
 package org.controllibrary.dtos.users;
 
+import org.controllibrary.utils.annotations.validations.emailConstraint.EmailConstraint;
+
 public record CreateUserDTO(
         String username,
+
+        @EmailConstraint
         String email,
+
         String password
 ) {
 }
