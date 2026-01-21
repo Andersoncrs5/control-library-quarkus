eb4cd6549fcdfb233fa9a6541e81ab8fbbbfb4d9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-21 10:12:32 -0300

    Impled UserRepository with method existsByEmail

---
+++ src/main/java/org/control/library/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package org.control.library.repositories;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.control.library.models.UserModel;
+import org.control.library.utils.annotations.valids.globals.EmailConstraint;
+
+@ApplicationScoped
+public class UserRepository implements PanacheRepository<UserModel> {
+
+    public Boolean existsByEmail(@EmailConstraint String email) {
+        return find("LOWER(email) = LOWER(?1)", email)
+                .count() > 0;
+    }
+
+}
