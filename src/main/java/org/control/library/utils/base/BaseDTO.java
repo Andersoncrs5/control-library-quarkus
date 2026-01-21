a4ea4b0f797c48adbc61ede7fb05f5d2005b1bbd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-21 11:01:38 -0300

    Impled BaseDTO

---
+++ src/main/java/org/control/library/utils/base/BaseDTO.java
@@ -0,0 +1,42 @@
+package org.control.library.utils.base;
+
+import java.time.OffsetDateTime;
+
+public abstract class BaseDTO {
+    private Long id;
+    private Long version;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
