abb1cf1b90f78c1ffc9cc51d3d150a8b3e097d5a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-21 10:12:40 -0300

    New exception ModelNotFoundException

---
+++ src/main/java/org/control/library/utils/exception/ModelNotFoundException.java
@@ -0,0 +1,8 @@
+package org.control.library.utils.exception;
+
+public class ModelNotFoundException extends RuntimeException {
+    public ModelNotFoundException(String message) {
+        super(message);
+    }
+}
+
