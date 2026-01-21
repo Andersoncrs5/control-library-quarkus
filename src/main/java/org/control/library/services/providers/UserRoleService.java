6f9f71829add340d32322b79afdd47c345a75b61
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-21 16:07:00 -0300

    Set UserRoleService interface and provider

---
+++ src/main/java/org/control/library/services/providers/UserRoleService.java
@@ -0,0 +1,9 @@
+package org.control.library.services.providers;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.control.library.services.interfaces.IUserRoleService;
+
+@ApplicationScoped
+public class UserRoleService implements IUserRoleService {
+
+}
