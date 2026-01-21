91056ac31e53382a31e62022953457edec98ee3a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-21 13:54:11 -0300

    Added annotation UniqueEmailUser in column email

--- src/main/java/org/control/library/dto/users/CreateUserDTO.java
+++ src/main/java/org/control/library/dto/users/CreateUserDTO.java
@@ -1,8 +1,13 @@
 package org.control.library.dto.users;
 
+import org.control.library.utils.annotations.valids.user.uniqueEmailUser.UniqueEmailUser;
+
 public record CreateUserDTO(
         String username,
+
+        @UniqueEmailUser
         String email,
+
         String password
 ) {
 }
