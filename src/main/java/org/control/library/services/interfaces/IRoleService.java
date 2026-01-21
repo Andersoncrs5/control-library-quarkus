289d7ed0bd3787d6bbc62a0bc2e43fe7a59499c1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-21 15:47:46 -0300

    Impled RoleService interface and provider

---
+++ src/main/java/org/control/library/services/interfaces/IRoleService.java
@@ -0,0 +1,15 @@
+package org.control.library.services.interfaces;
+
+import jakarta.validation.constraints.NotBlank;
+import org.control.library.models.RoleModel;
+import org.control.library.utils.annotations.valids.globals.isModelInitialized.IsModelInitialized;
+
+import java.util.Optional;
+
+public interface IRoleService {
+    Boolean existsByName(@NotBlank String name);
+    RoleModel getByNameSimple(@NotBlank String name);
+    Optional<RoleModel> getByName(@NotBlank String name);
+    void delete(@IsModelInitialized RoleModel role);
+    RoleModel create(RoleModel role);
+}
---
+++ src/main/java/org/control/library/services/providers/RoleService.java
@@ -0,0 +1,42 @@
+package org.control.library.services.providers;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotBlank;
+import org.control.library.models.RoleModel;
+import org.control.library.repositories.RoleRepository;
+import org.control.library.services.interfaces.IRoleService;
+import org.control.library.utils.annotations.valids.globals.isModelInitialized.IsModelInitialized;
+import org.control.library.utils.exception.ModelNotFoundException;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class RoleService implements IRoleService {
+
+    @Inject
+    private RoleRepository repository;
+
+    public Boolean existsByName(@NotBlank String name) {
+        return this.repository.existsByName(name);
+    }
+
+    public RoleModel getByNameSimple(@NotBlank String name) {
+        return repository.findByName(name)
+                .orElseThrow(() -> new ModelNotFoundException("Role not found"));
+    }
+
+    public Optional<RoleModel> getByName(@NotBlank String name) {
+        return repository.findByName(name);
+    }
+
+    public void delete(@IsModelInitialized RoleModel role) {
+        this.repository.delete(role);
+    }
+
+    public RoleModel create(RoleModel role) {
+        this.repository.persist(role);
+        return role;
+    }
+
+}
