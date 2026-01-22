fa1770c527cf33ae1915b2a1db6a4f65080f578c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-21 21:07:40 -0300

    Added annotation NotBlank in method encode and verify

--- src/main/java/org/control/library/configs/security/CryptoService.java
+++ src/main/java/org/control/library/configs/security/CryptoService.java
@@ -2,17 +2,18 @@ package org.control.library.configs.security;
 
 import com.password4j.Password;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.validation.constraints.NotBlank;
 
 @ApplicationScoped
 public class CryptoService {
 
-    public String encode(String rawPassword) {
+    public String encode(@NotBlank String rawPassword) {
         return Password.hash(rawPassword)
                 .withArgon2()
                 .getResult();
     }
 
-    public boolean verify(String rawPassword, String encodedPassword) {
+    public boolean verify(@NotBlank String rawPassword, @NotBlank String encodedPassword) {
         return Password.check(rawPassword, encodedPassword).withArgon2();
     }
 }
