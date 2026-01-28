fe8ed756e489bfd433f80d2cb240a64350a23886
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-28 13:43:27 -0300

    New exception NotAuthenticatedException

---
+++ src/main/java/org/control/library/utils/exception/NotAuthenticatedException.java
@@ -0,0 +1,7 @@
+package org.control.library.utils.exception;
+
+public class NotAuthenticatedException extends RuntimeException {
+    public NotAuthenticatedException(String message) {
+        super(message);
+    }
+}
