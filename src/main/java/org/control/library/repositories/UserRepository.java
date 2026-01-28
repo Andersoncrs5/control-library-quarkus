ad1f8b165d46744d4c48de6a456606240336eecf
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-28 11:14:52 -0300

    New method findByEmail

--- src/main/java/org/control/library/repositories/UserRepository.java
+++ src/main/java/org/control/library/repositories/UserRepository.java
@@ -5,12 +5,18 @@ import jakarta.enterprise.context.ApplicationScoped;
 import org.control.library.models.UserModel;
 import org.control.library.utils.annotations.valids.globals.EmailConstraint;
 
+import java.util.Optional;
+
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<UserModel> {
 
     public Boolean existsByEmail(@EmailConstraint String email) {
         return find("LOWER(email) = LOWER(?1)", email)
                 .count() > 0;
+    }
+
+    public Optional<UserModel> findByEmail(@EmailConstraint String email) {
+        return find("LOWER(email) = LOWER(?1)", email).firstResultOptional();
     }
 
 }
