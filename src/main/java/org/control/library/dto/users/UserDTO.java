fba155c1ee836f4f9251eeb4229e2ec11c83ec92
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-28 11:15:03 -0300

    New gets and sets

--- src/main/java/org/control/library/dto/users/UserDTO.java
+++ src/main/java/org/control/library/dto/users/UserDTO.java
@@ -9,30 +9,12 @@ public class UserDTO extends BaseDTO {
     private String email;
     private OffsetDateTime blockedAt;
 
-    public UserDTO() {
-    }
+    public UserDTO() {}
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public OffsetDateTime getBlockedAt() {
-        return blockedAt;
-    }
-
-    public void setBlockedAt(OffsetDateTime blockedAt) {
-        this.blockedAt = blockedAt;
-    }
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public OffsetDateTime getBlockedAt() {return blockedAt;}
+    public void setBlockedAt(OffsetDateTime blockedAt) {this.blockedAt = blockedAt;}
 }
